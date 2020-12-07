0b6f6a9e91cc35af20f12dfddca16c7bcbf93876
Jordan <jordanabid@gmail.com>
2020-12-07 10:31:42 +0100

    Init JPA

---
+++ DecouverteJPA/src/main/java/model/Moto.java
@@ -0,0 +1,46 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Moto {
+	
+	@Id
+	private Integer id;
+	private String marque;
+	private int vitesse;
+	
+	
+	public Moto() {
+	}
+
+
+	public Moto(String marque, int vitesse) {
+		this.setMarque(marque);
+		this.setVitesse(vitesse);
+	}
+
+
+	public String getMarque() {
+		return marque;
+	}
+
+
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+
+
+	public int getVitesse() {
+		return vitesse;
+	}
+
+
+	public void setVitesse(int vitesse) {
+		this.vitesse = vitesse;
+	}
+	
+	
+
+}
