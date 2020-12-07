60240016282e1b7a86080353d5d2708cb5cb0001
Jordan <jordanabid@gmail.com>
2020-12-07 14:30:30 +0100

    Entity

--- DecouverteJPA/src/main/java/test/App.java
+++ DecouverteJPA/src/main/java/test/App.java
@@ -1,5 +1,6 @@
 package test;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,6 +9,15 @@ public class App {
 	public static void main(String[] args) {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("baseDemoJPA");
+		
+		//EntityManager em = emf.createEntityManager();
+		
+		//em.getTransaction().begin();
+		
+		//em.getTransaction().commit();
+		
+		
+		//em.close();
 		emf.close();
 		
 	}
