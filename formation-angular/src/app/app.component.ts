4f93861523cd38b5621f20353b01c81f4d4be67b
oliviergoz <o.gozlan@ajc-ingenierie.fr>
2021-02-04 16:58:53 +0100

    exo1

--- formation-angular/src/app/app.component.ts
+++ formation-angular/src/app/app.component.ts
@@ -8,13 +8,5 @@ import { Client } from 'src/model/client';
 })
 export class AppComponent {
   title = 'formation-angular';
-  client: Client = new Client();
-  message: string = '';
-  couleur = 'red';
-  inconnu = 'je sais pas qui ';
-
-  public bouton() {
-    console.log('il fait beau aujourdhui');
-    this.message = 'bravo tu as cliqué sur le bouton';
-  }
+  couleur = 'blue';
 }
